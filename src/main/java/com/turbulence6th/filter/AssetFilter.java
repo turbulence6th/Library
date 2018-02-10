26ce872e41c468bd77d870a690cb8207b2d678f1
turbulence6th <turbulence6th@gmail.com>
2018-02-10 09:29:16 +0300

    main page made

--- src/main/java/com/turbulence6th/filter/AssetFilter.java
+++ src/main/java/com/turbulence6th/filter/AssetFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -24,6 +25,16 @@ public class AssetFilter implements Filter {
 		    request.getRequestDispatcher("/pages" + path).forward(request, response);
 		}
 		
+	}
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		
+	}
+	
+	@Override
+	public void destroy() {
+	
 	}
 
 }
